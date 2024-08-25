68d191a72a828abafd08c28192f92ba0ae28bd94
Lenus Walker <walker.lenus@gmail.com>
2024-08-25 11:56:43 -0400

    testing displaylink evdi build

--- build-displaylink-rpm.sh
+++ build-displaylink-rpm.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 rpm-ostree install rpmdevtools gcc gcc-c++ gcc-plugin-annobin libdrm libdrm-devel make \
     gnome-session-wayland-session xorg-x11-server-Xwayland python3-rpkg fedpkg webkitgtk6.0 gdm \
-    webkit2gtk4 gnome-shell gnome-initial-setup
+    gnome-shell gnome-initial-setup
 
 git clone https://github.com/displaylink-rpm/displaylink-rpm.git /tmp/displaylink-rpm
 
@@ -14,4 +14,4 @@ make rawhide
 
 rpm-ostree uninstall rpmdevtools gcc gcc-c++ gcc-plugin-annobin libdrm libdrm-devel make \
     gnome-session-wayland-session xorg-x11-server-Xwayland python3-rpkg fedpkg webkitgtk6.0 gdm \
-    webkit2gtk4 gnome-shell gnome-initial-setup
+    gnome-shell gnome-initial-setup
