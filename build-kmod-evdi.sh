04f177d741b33ef4e7bee5e1c9a87d02d3b58fa4
Lenus Walker <walker.lenus@gmail.com>
2024-08-25 17:13:15 -0400

    testing evdi build

--- build-kmod-evdi.sh
+++ build-kmod-evdi.sh
@@ -6,13 +6,14 @@ set -ouex pipefail
 ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q kernel)"
 RELEASE="$(rpm -E '%fedora')"
+RELEASE=40
 
 cp /tmp/akmods-addons/rpmbuild/SOURCES/negativo17-fedora-multimedia.repo /etc/yum.repos.d/
 
 ### BUILD evdi (succeed or fail-fast with debug output)
 export CFLAGS="-fno-pie -no-pie"
-dnf install -y \
-    akmod-evdi-*.fc40.${ARCH}
+rpm-ostree install -y \
+    akmod-evdi-1.14.6-2.fc40.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod evdi
 modinfo /usr/lib/modules/${KERNEL}/extra/evdi/evdi.ko.xz > /dev/null \
 || (find /var/cache/akmods/evdi/ -name \*.log -print -exec cat {} \; && exit 1)
