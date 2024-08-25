f870dae586ed6be4cfcf4317174fe0f8af9c8826
Lenus Walker <walker.lenus@gmail.com>
2024-08-25 11:53:30 -0400

    correct script error

--- build-displaylink-rpm.sh
+++ build-displaylink-rpm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ouex pipefail
+
 rpm-ostree install rpmdevtools gcc gcc-c++ gcc-plugin-annobin libdrm libdrm-devel make \
     gnome-session-wayland-session xorg-x11-server-Xwayland python3-rpkg fedpkg webkitgtk6.0 gdm \
     webkit2gtk4 gnome-shell gnome-initial-setup
